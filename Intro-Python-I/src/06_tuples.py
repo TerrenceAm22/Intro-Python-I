7091d43638f285cd97eab407a275c0d60b79896c
TerrenceAm22 <tamalone87@gmail.com>
2020-09-01 16:16:04 -0400

    tuples at are defeated

--- Intro-Python-I/src/06_tuples.py
+++ Intro-Python-I/src/06_tuples.py
@@ -35,8 +35,9 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 # Write a function `print_tuple` that prints all the values in a tuple
 
 def print_tuple(t):
-    print([t[0] for num in t])
+    print(t[:])
     return
+
     
    
 
@@ -47,5 +48,8 @@ t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?\
+print("By adding a comma to it")
 print_tuple(u)
+
+
